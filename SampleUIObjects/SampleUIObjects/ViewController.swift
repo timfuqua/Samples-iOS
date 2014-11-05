79a7027b4ab98d7ba5aaba78ea4ee5a513c2cbc4
Tim Fuqua <timfuqua@gmail.com>
2014-11-04 18:37:45 -0800

    Adding the dynamically created UI Objects to the sample project. Objects are created in the viewDidLoad function.

--- SampleUIObjects/SampleUIObjects/ViewController.swift
+++ SampleUIObjects/SampleUIObjects/ViewController.swift
@@ -14,14 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let canvas = UIView(frame: CGRectMake(0, 0, 200, 200))
+        canvas.backgroundColor = UIColor.blueColor()
         
+        let title = UILabel(frame: CGRectMake(50, 50, 100, 20))
+        title.text = "My Title"
+        title.textColor = UIColor.whiteColor()
+        title.textAlignment = NSTextAlignment.Center
+        
+        let button = UIButton(frame: CGRectMake(50, 100, 100, 50))
+        button.backgroundColor = UIColor.whiteColor()
+        button.setTitle("My Button", forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        
+        button.layer.cornerRadius = 10
+        button.layer.shadowColor = UIColor.blackColor().CGColor
+        button.layer.shadowOpacity = 0.1
+        button.layer.shadowOffset = CGSizeMake(10, 10)
+        button.layer.shadowRadius = 10
+        
+        canvas.addSubview(button)
+        canvas.addSubview(title)
+        
+        self.view.addSubview(canvas)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
