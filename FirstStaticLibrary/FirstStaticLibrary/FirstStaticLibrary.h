944ccf8f7b20313f195fe65a4651284307e63f5e
Tim Fuqua <timfuqua@gmail.com>
2015-01-13 09:13:35 -0800

    Adding files for a first static library.

---
+++ FirstStaticLibrary/FirstStaticLibrary/FirstStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  FirstStaticLibrary.h
+//  FirstStaticLibrary
+//
+//  Created by Tim Fuqua on 12/30/14.
+//  Copyright (c) 2014 FuquaProductions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstStaticLibrary : NSObject
+
+@end
---
+++ FirstStaticLibrary/FirstStaticLibrary/FirstStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  FirstStaticLibrary.m
+//  FirstStaticLibrary
+//
+//  Created by Tim Fuqua on 12/30/14.
+//  Copyright (c) 2014 FuquaProductions. All rights reserved.
+//
+
+#import "FirstStaticLibrary.h"
+
+@implementation FirstStaticLibrary
+
+@end
