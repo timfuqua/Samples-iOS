467db718da430030b031075335d7194f9f9f6c3f
Tim Fuqua <timfuqua@gmail.com>
2015-01-13 09:13:13 -0800

    Adding files for a first framework.

---
+++ FirstFramework/FirstFramework/FirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by Tim Fuqua on 12/30/14.
+//  Copyright (c) 2014 FuquaProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+
