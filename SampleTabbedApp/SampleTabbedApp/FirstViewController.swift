23cb93b0647523fc0e0b52df2e8e204c558bfb73
Tim Fuqua <timfuqua@gmail.com>
2015-01-13 09:23:46 -0800

    Adding sample tabbed app.

---
+++ SampleTabbedApp/SampleTabbedApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SampleTabbedApp
+//
+//  Created by Tim Fuqua on 12/30/14.
+//  Copyright (c) 2014 FuquaProductions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SampleTabbedApp/SampleTabbedAppTests/SampleTabbedAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleTabbedAppTests.swift
+//  SampleTabbedAppTests
+//
+//  Created by Tim Fuqua on 12/30/14.
+//  Copyright (c) 2014 FuquaProductions. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SampleTabbedAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
