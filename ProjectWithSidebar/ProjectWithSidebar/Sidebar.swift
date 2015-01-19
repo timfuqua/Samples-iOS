253f111bdb737453203fda6ba0c5d66efd23c9fd
Tim Fuqua <timfuqua@gmail.com>
2015-01-19 09:59:21 -0800

    Adding sample project that implements a slide out navigation design pattern.

---
+++ ProjectWithSidebar/ProjectWithSidebar/Sidebar.swift
@@ -0,0 +1,35 @@
+//
+//  Sidebar.swift
+//  ProjectWithSidebar
+//
+//  Created by Tim Fuqua on 1/16/15.
+//  Copyright (c) 2015 FuquaProductions. All rights reserved.
+//
+
+import UIKit
+
+class Sidebar: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
