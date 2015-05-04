48bd4baf5547a2ccb85efbb60abed8e7f8dad422
Tim Fuqua <timfuqua@gmail.com>
2015-05-04 10:48:54 -0700

    Adding a playground using regular expressions.

---
+++ Playgrounds/Regex.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func matchesForRegexInText(regex: String!, text: String!) -> [String] {
+  
+  let regex = NSRegularExpression(pattern: regex, options: nil, error: nil)!
+  let nsString = text as NSString
+  let results = regex.matchesInString(text, options: nil, range: NSMakeRange(0, nsString.length)) as [NSTextCheckingResult]
+  return map(results) { nsString.substringWithRange($0.range)}
+}
+
+let str = "Hello, playground"
+let strMatches = matchesForRegexInText("[a-z]+ground$", str)
+println(strMatches)
+
+let filename = "Filename"
+let filenameMatches = matchesForRegexInText("[[0-9]+]$", filename)
+println(filenameMatches)
+
