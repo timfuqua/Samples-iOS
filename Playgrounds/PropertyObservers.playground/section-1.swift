f4a1474b40bfc754ae7b0acd549e64c2a0fc62b2
Tim Fuqua <timfuqua@gmail.com>
2015-05-06 17:15:52 -0700

    Updating the PropertyObservers playground with code that shows that changing the properties of an instantiation of a class object B within a class A will not trigger the willSet and didSet property observers that class A set up on the class B object it owns. Assigning to the class B object as a whole will.

--- Playgrounds/PropertyObservers.playground/section-1.swift
+++ Playgrounds/PropertyObservers.playground/section-1.swift
@@ -4,6 +4,7 @@ import UIKit
 
 class Thing {
 
+  var someInt: Int = 0
   var someArray: [Int] = [] {
     willSet {
       println("Thing: willSet \(someArray) to \(newValue)")
@@ -16,31 +17,67 @@ class Thing {
   
 }
 
-var t = Thing()
-t.someArray.append(1)
-t.someArray.append(2)
-t.someArray.append(3)
-
-t.someArray[0] = 10
-t.someArray[1] = 20
-t.someArray[2] = 30
-
-
-
-var otherArray: [Int] = [] {
-  willSet {
-    println("willSet \(otherArray) to \(newValue)")
+class HasAThing {
+  
+  var myThing: Thing = Thing() {
+    willSet {
+      println("HasAThing: willSet \(myThing) to \(newValue)")
+    }
+    
+    didSet {
+      println("HasAThing: didSet \(oldValue) to \(myThing)")
+    }
   }
   
-  didSet {
-    println("didSet \(oldValue) to \(otherArray)")
-  }
 }
 
-otherArray.append(10)
-otherArray.append(20)
-otherArray.append(30)
+func test1() {
+  var t = Thing()
+  t.someArray.append(1)
+  t.someArray.append(2)
+  t.someArray.append(3)
+  
+  t.someArray[0] = 10
+  t.someArray[1] = 20
+  t.someArray[2] = 30
+}
 
-otherArray[0] = 100
-otherArray[1] = 200
-otherArray[2] = 300
+//func test2() {
+  var otherArray: [Int] = [] {
+    willSet {
+      println("willSet \(otherArray) to \(newValue)")
+    }
+    
+    didSet {
+      println("didSet \(oldValue) to \(otherArray)")
+    }
+  }
+  
+  otherArray.append(10)
+  otherArray.append(20)
+  otherArray.append(30)
+  
+  otherArray[0] = 100
+  otherArray[1] = 200
+  otherArray[2] = 300
+//}
+  
+func test3() {
+  var hat = HasAThing()
+  hat.myThing.someInt = 1
+  
+  var otherThing = Thing()
+  
+  hat.myThing = otherThing
+}
+
+test1()
+//test2()
+test3()
+
+
+
+
+
+
+
