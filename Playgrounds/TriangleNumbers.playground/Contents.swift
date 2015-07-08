355d017624eb01b46afdcdd4e10c04ff9824fa21
Tim Fuqua <timfuqua@gmail.com>
2015-07-08 16:12:14 -0700

    Updating the Combinations playground. Adding the start of a new playground for the TriangleNumbers game.

---
+++ Playgrounds/TriangleNumbers.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Given a set of 3 numbers chosen randomly between a and b, how many
+// different left-to-right orderings of the 3 numbers can you make?
+
+// IE: if a=0 and b=0, you can make exactly one ordering {0,0,0}
+//  if a=0 and b=1, you can do the following:
+//    let x:=the first number
+//    let y:=the second number
+//    let z:=the third number
+//    
+//    The possible permutations of x, y, and z ({x,y,z}) are:
+//      {0,0,0}, {0,0,1}, {0,1,0}, {0,1,1}, {1,0,0}, {1,0,1},
+
+import UIKit
+import Foundation
+
+
+
+
+
