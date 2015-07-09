2225d90b77da07a9b98ad952ebf4932c3d16ecd4
Tim Fuqua <timfuqua@gmail.com>
2015-07-09 16:16:51 -0700

    Updated wording from Permutation to Combination, since that's more accurate.

--- Playgrounds/TriangleNumbers.playground/Contents.swift
+++ Playgrounds/TriangleNumbers.playground/Contents.swift
@@ -8,10 +8,10 @@
 //    let y:=the second number
 //    let z:=the third number
 //    
-//    The 8 possible permutations of x, y, and z ({x,y,z}) are binary patterns:
+//    The 8 possible combinations of x, y, and z ({x,y,z}) are binary patterns:
 //      {0,0,0}, {0,0,1}, {0,1,0}, {0,1,1}, {1,0,0}, {1,0,1}, {1,1,0}, {1,1,1}
 //
-//    We also want to strip out any permutations where {x,y,z} = {y,z,x} = {z,x,y}
+//    We also want to strip out any combinations where {x,y,z} = {y,z,x} = {z,x,y}
 //
 //    We can ignore any case where x=y=z. Starting at the beginning, skip {0,0,0},
 //    and generate the 2 shifted cases of {0,0,1} ({1,0,0} and {0,1,0}) and
@@ -22,17 +22,17 @@
 //    and strip them out of the set:
 //      {0,0,0}, {0,0,1}, {0,1,1}, {1,1,1}
 //
-//    We are left with just 4 permutations
+//    We are left with just 4 combinations
 
 // IE: if a=1 and b=3, you can do the following
 //    let x:=the first number
 //    let y:=the second number
 //    let z:=the third number
 //
-//    The 27 possible permutations of x, y, and z ({x,y,z}) are:
+//    The 27 possible combinations of x, y, and z ({x,y,z}) are:
 //      {1,1,1}, {1,1,2}, {1,1,3}, {1,2,1}, {1,2,2}, {1,2,3}, {1,3,1}, {1,3,2}, {1,3,3}, {2,1,1}, {2,1,2}, {2,1,3}, {2,2,1}, {2,2,2}, {2,2,3}, {2,3,1}, {2,3,2}, {2,3,3}, {3,1,1}, {3,1,2}, {3,1,3}, {3,2,1}, {3,2,2}, {3,2,3}, {3,3,1}, {3,3,2}, {3,3,3}
 //
-//    We then strip out any permutations where {x,y,z} = {y,z,x} = {z,x,y}
+//    We then strip out any combinations where {x,y,z} = {y,z,x} = {z,x,y}
 //
 //    We can ignore any case where x=y=z. Starting at the beginning:
 //
@@ -71,21 +71,21 @@
 //    Skip {3,3,3}.
 //
 
-// A better algorithm would be to check if the permutation about to be added is
+// A better algorithm would be to check if the combination about to be added is
 // a shift variant of an existing item in the list or not. Only add the new
-// permutation if a shift variant doesn't exist.
+// combination if a shift variant doesn't exist.
 
 // Another thought is that if there is a mathematical formula for determining
 // the positions of the shift variants in the unfiltered generated list, then
 // trimming the final list would be trivial and there would be no need to
-// perform any such logic when generating the permutations to see if they were
-// shift variants of existing permutations already in the list.
+// perform any such logic when generating the combinations to see if they were
+// shift variants of existing combinations already in the list.
 
 // When a=0, b=1, that means there are 2 possible values. We are choosing them
-// for x,y,z, which is 3 values. 2^3 = 8 possible permutations
+// for x,y,z, which is 3 values. 2^3 = 8 possible combinations
 //
 // When a=1, b=3, that means there are 3 possible values. We are choosing them
-// for x,y,z, which is 3 values. 3^3 = 27 possible permutations
+// for x,y,z, which is 3 values. 3^3 = 27 possible combinations
 
 
 
@@ -110,42 +110,42 @@ func reduceWithDelimiter(stringArray: [String], delimiter: String) -> String {
   }), count(delimiter))
 }
 
-// treat the values in a Permutation like the digits in a number system where
+// treat the values in a Combination like the digits in a number system where
 // each digit is an element of [a,b]. For example, a 3 digit number can range
 // from {a,a,a} to {b,b,b}. The second lowest number is {a,a,a+1} and the
 // second largest number is {b,b,b-1}. This is equivalent to the decimal numbers
-// of 001 and 998, where a=0 and b=9. When incrementing the permutation, add one
+// of 001 and 998, where a=0 and b=9. When incrementing the combination, add one
 // to the value at the last index. If it's value would go over the upper bound,
 // add 1 to the previous index and repeat the check until the digit doesn't go
 // above the upper bound. If the value at index 0 would go over the upper bound,
 // then you've found the 2's compliment of this number system and have "rolled
 // over" to the lowest number.
-postfix func ++(inout permutation: Permutation) -> Permutation {
+postfix func ++(inout combination: Combination) -> Combination {
   let extraDebugging: Bool = false
-  if extraDebugging { println("Starting postfix++: \(permutation.toString())") }
-  var originalPermutation = permutation
+  if extraDebugging { println("Starting postfix++: \(combination.toString())") }
+  var originalCombination = combination
   
-  if extraDebugging { println(permutation.values.count-1) }
-  for var i = permutation.values.count-1; i >= 0; i-- {
+  if extraDebugging { println(combination.values.count-1) }
+  for var i = combination.values.count-1; i >= 0; i-- {
     if extraDebugging { println("Starting loop at i=\(i)") }
-    let newVal = permutation.values[i]+1
+    let newVal = combination.values[i]+1
     if extraDebugging { println("newVal will be: \(newVal)") }
-    if newVal > permutation.settings.upperBound {
-      if extraDebugging { println("newVal > permutation.upperBound: \(newVal) > \(permutation.settings.upperBound)") }
-      permutation.values[i] = permutation.settings.lowerBound
+    if newVal > combination.settings.upperBound {
+      if extraDebugging { println("newVal > combination.upperBound: \(newVal) > \(combination.settings.upperBound)") }
+      combination.values[i] = combination.settings.lowerBound
     }
     else {
-      if extraDebugging { println("newVal <= permutation.upperBound: \(newVal) <= \(permutation.settings.upperBound)") }
-      permutation.values[i] = newVal
+      if extraDebugging { println("newVal <= combination.upperBound: \(newVal) <= \(combination.settings.upperBound)") }
+      combination.values[i] = newVal
       break
     }
   }
   
-  if extraDebugging { println("Returning postfix++: \(originalPermutation.toString())") }
-  return originalPermutation
+  if extraDebugging { println("Returning postfix++: \(originalCombination.toString())") }
+  return originalCombination
 }
 
-struct PermutationSettings {
+struct CombinationSettings {
   var lowerBound: Int
   var upperBound: Int
   var numValues: Int
@@ -157,11 +157,11 @@ struct PermutationSettings {
   }
 }
 
-struct Permutation {
-  var settings: PermutationSettings
+struct Combination {
+  var settings: CombinationSettings
   var values: [Int] = []
   
-  init(settings: PermutationSettings, values: [Int]? = nil) {
+  init(settings: CombinationSettings, values: [Int]? = nil) {
     self.settings = settings
     if values != nil {
       if values!.count == settings.numValues {
@@ -203,8 +203,8 @@ struct Permutation {
   }
 }
 
-func createLowestPermutation(settings: PermutationSettings) -> Permutation {
-  var lowestPerm: Permutation = Permutation(settings: settings)
+func createLowestCombination(settings: CombinationSettings) -> Combination {
+  var lowestPerm: Combination = Combination(settings: settings)
   
   for i in 0..<lowestPerm.settings.numValues {
     lowestPerm.values.append(lowestPerm.settings.lowerBound)
@@ -213,8 +213,8 @@ func createLowestPermutation(settings: PermutationSettings) -> Permutation {
   return lowestPerm
 }
 
-func createHighestPermutation(settings: PermutationSettings) -> Permutation {
-  var highestPerm: Permutation = Permutation(settings: settings)
+func createHighestCombination(settings: CombinationSettings) -> Combination {
+  var highestPerm: Combination = Combination(settings: settings)
   
   for i in 0..<highestPerm.settings.numValues {
     highestPerm.values.append(highestPerm.settings.upperBound)
@@ -224,16 +224,16 @@ func createHighestPermutation(settings: PermutationSettings) -> Permutation {
 }
 
 func test01() {
-  println("Create a 3 digit permutation and display it's value")
-  var aPerm = Permutation(settings: PermutationSettings(lowerBound: 1, upperBound: 3, numValues: 3), values: [1,2,3])
+  println("Create a 3 digit combination and display it's value")
+  var aPerm = Combination(settings: CombinationSettings(lowerBound: 1, upperBound: 3, numValues: 3), values: [1,2,3])
   
   println(aPerm.toString())
 }
 
-func generateAllPermutations(settings: PermutationSettings) -> [Permutation] {
-  var allPerms: [Permutation] = []
-  let firstPerm = createLowestPermutation(settings)
-  let lastPerm = createHighestPermutation(settings)
+func generateAllCombinations(settings: CombinationSettings) -> [Combination] {
+  var allPerms: [Combination] = []
+  let firstPerm = createLowestCombination(settings)
+  let lastPerm = createHighestCombination(settings)
   var incrementingPerm = firstPerm
 
   while incrementingPerm.toString() != lastPerm.toString() {
@@ -245,8 +245,8 @@ func generateAllPermutations(settings: PermutationSettings) -> [Permutation] {
 }
 
 func test02() {
-  println("Create a 3 digit permutation and perform some increment operations")
-  var aPerm = Permutation(settings: PermutationSettings(lowerBound: 0, upperBound: 1, numValues: 3), values: [0,0,0])
+  println("Create a 3 digit combination and perform some increment operations")
+  var aPerm = Combination(settings: CombinationSettings(lowerBound: 0, upperBound: 1, numValues: 3), values: [0,0,0])
   
   println("should be {0,0,0}")
   println(aPerm.toString())
@@ -261,52 +261,52 @@ func test02() {
 }
 
 func test03() {
-  println("Determine all the 3 digit permutations of [0,1]")
-  var allPerms: [Permutation] = generateAllPermutations(PermutationSettings(lowerBound: 0, upperBound: 1, numValues: 3))
+  println("Determine all the 3 digit combinations of [0,1]")
+  var allPerms: [Combination] = generateAllCombinations(CombinationSettings(lowerBound: 0, upperBound: 1, numValues: 3))
   
-  println("Total number of permutations: \(allPerms.count)")
+  println("Total number of combinations: \(allPerms.count)")
   println(reduceWithDelimiter(allPerms.map({ $0.toString() }), ", "))
 }
 
 func test04() {
-  println("Determine all the 3 digit permutations of [1,3]")
-  var allPerms: [Permutation] = generateAllPermutations(PermutationSettings(lowerBound: 1, upperBound: 3, numValues: 3))
+  println("Determine all the 3 digit combinations of [1,3]")
+  var allPerms: [Combination] = generateAllCombinations(CombinationSettings(lowerBound: 1, upperBound: 3, numValues: 3))
   
-  println("Total number of permutations: \(allPerms.count)")
+  println("Total number of combinations: \(allPerms.count)")
   println(reduceWithDelimiter(allPerms.map({ $0.toString() }), ", "))
 }
 
 func test05() {
-  println("Determine all the 3 digit permutations of [1,4]")
-  var allPerms: [Permutation] = generateAllPermutations(PermutationSettings(lowerBound: 1, upperBound: 4, numValues: 3))
+  println("Determine all the 3 digit combinations of [1,4]")
+  var allPerms: [Combination] = generateAllCombinations(CombinationSettings(lowerBound: 1, upperBound: 4, numValues: 3))
   
-  println("Total number of permutations: \(allPerms.count)")
+  println("Total number of combinations: \(allPerms.count)")
   println(reduceWithDelimiter(allPerms.map({ $0.toString() }), ", "))
 }
 
-// takes in a list of permutations that presumably was generated using the
-// generateAllPermutations function, and returns an array of indices of where
-// the shift invariant permutations are. Shift invariant permutations are of the
+// takes in a list of combinations that presumably was generated using the
+// generateAllCombinations function, and returns an array of indices of where
+// the shift invariant combinations are. Shift invariant combinations are of the
 // form {x,x,x,...,x}, where every value in the tuple is the same.
-func getShiftInvariantIndices(fromPermList: [Permutation]) -> [Int] {
-  // if the supplied list has no permutations, return an empty set
+func getShiftInvariantIndices(fromPermList: [Combination]) -> [Int] {
+  // if the supplied list has no combinations, return an empty set
   if fromPermList.count == 0 {
     return []
   }
   
-  // if the supplied list has exactly one permutation, which would be the case
+  // if the supplied list has exactly one combination, which would be the case
   // if the list was generated with lowerBound = upperBound, then the only entry
-  // is a shift invariant permutation
+  // is a shift invariant combination
   if fromPermList.count == 1 {
     return [0]
   }
   
-  // starting at the lower bound of the permutation, go through each permutation
+  // starting at the lower bound of the combination, go through each combination
   // until you find one that is shift invariant and it's value *should* match
   // the currentValue, then increment the current value and continue searching
-  // for the next shift invariant permutation.
-  // The first permutation should be shift invariant with a value of lowerBound
-  // and the last permutation should be shift invariant with a value of
+  // for the next shift invariant combination.
+  // The first combination should be shift invariant with a value of lowerBound
+  // and the last combination should be shift invariant with a value of
   // upperBound, but that can be done only as an assurance test.
   var currentValue: Int = fromPermList[0].settings.lowerBound
   var shiftInvariantIndices: [Int] = []
@@ -323,14 +323,14 @@ func getShiftInvariantIndices(fromPermList: [Permutation]) -> [Int] {
 }
 
 func test06() {
-  println("Determine all the 3 digit permutations of [1,4], and determine the indices of the shift invariant permutations")
-  var allPerms: [Permutation] = generateAllPermutations(PermutationSettings(lowerBound: 1, upperBound: 4, numValues: 3))
+  println("Determine all the 3 digit combinations of [1,4], and determine the indices of the shift invariant combinations")
+  var allPerms: [Combination] = generateAllCombinations(CombinationSettings(lowerBound: 1, upperBound: 4, numValues: 3))
   
-  println("Total number of permutations: \(allPerms.count)")
+  println("Total number of combinations: \(allPerms.count)")
   println(reduceWithDelimiter(allPerms.map({ $0.toString() }), ", "))
 
   var shiftInvariantIndices = getShiftInvariantIndices(allPerms)
-  println("Total number of shift invariant permutations: \(shiftInvariantIndices.count)")
+  println("Total number of shift invariant combinations: \(shiftInvariantIndices.count)")
   for i in 0..<shiftInvariantIndices.count {
     println("At index \(shiftInvariantIndices[i]): " + allPerms[shiftInvariantIndices[i]].toString())
   }
@@ -339,14 +339,14 @@ func test06() {
 }
 
 func testFindShiftInvariance(lowerBound: Int, upperBound: Int, numValues: Int) {
-  println("Determine all the \(numValues) digit permutations of [\(lowerBound),\(upperBound)], and determine the indices of the shift invariant permutations")
-  var allPerms: [Permutation] = generateAllPermutations(PermutationSettings(lowerBound: lowerBound, upperBound: upperBound, numValues: numValues))
+  println("Determine all the \(numValues) digit combinations of [\(lowerBound),\(upperBound)], and determine the indices of the shift invariant combinations")
+  var allPerms: [Combination] = generateAllCombinations(CombinationSettings(lowerBound: lowerBound, upperBound: upperBound, numValues: numValues))
   
-  println("Total number of permutations: \(allPerms.count)")
+  println("Total number of combinations: \(allPerms.count)")
   println(reduceWithDelimiter(allPerms.map({ $0.toString() }), ", "))
   
   var shiftInvariantIndices = getShiftInvariantIndices(allPerms)
-  println("Total number of shift invariant permutations: \(shiftInvariantIndices.count)")
+  println("Total number of shift invariant combinations: \(shiftInvariantIndices.count)")
   for i in 0..<shiftInvariantIndices.count {
     println("At index \(shiftInvariantIndices[i]): " + allPerms[shiftInvariantIndices[i]].toString())
   }
@@ -356,13 +356,18 @@ func testFindShiftInvariance(lowerBound: Int, upperBound: Int, numValues: Int) {
 
 func test07() {
   var lowerBound: Int = 1
-  var highestUpperBound: Int = 6
+  var highestUpperBound: Int = 3
+//  var highestUpperBound: Int = 6
   var numDigits: Int = 3
   
   for i in lowerBound...highestUpperBound {
     testFindShiftInvariance(lowerBound, i, 3)
   }
 }
+
+//func removeShiftVariants(fromPermList: [Combination]) -> [Combination] {
+//  
+//}
 
 //test01()
 //test02()
