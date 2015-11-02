5d94ad011fd6fa32f048bbf01d8ab76078eacbcd
Tim Fuqua <timfuqua@gmail.com>
2015-11-02 14:51:35 -0800

    Functionalizing the StringSplitting playground.

--- Playgrounds/StringSplitting.playground/section-1.swift
+++ Playgrounds/StringSplitting.playground/section-1.swift
@@ -2,32 +2,52 @@
 
 import UIKit
 
-var myString = "Here is my string"
-var myArray : [String] = myString.componentsSeparatedByString(" ")
-//Returns a string array with the following values:  ["Here", "is", "my", "string"]
-
-var someNumbersInAString: String = "0 0 200 200"
-var someNumbersInAStringArray: [String] = someNumbersInAString.componentsSeparatedByString(" ")
-var someNumbersInAnIntArray: [Int] = []
-
-for values in someNumbersInAStringArray {
+func test01() {
+  var myString = "Here is my string"
+  var myArray : [String] = myString.componentsSeparatedByString(" ")
+  //Returns a string array with the following values:  ["Here", "is", "my", "string"]
+  
+  var someNumbersInAString: String = "0 0 200 200"
+  var someNumbersInAStringArray: [String] = someNumbersInAString.componentsSeparatedByString(" ")
+  var someNumbersInAnIntArray: [Int] = []
+  
+  for values in someNumbersInAStringArray {
     someNumbersInAnIntArray.append(values.toInt()!)
+  }
+  
+  println(someNumbersInAnIntArray)
+  
+  var someNumbersInAFourTuple: (Int, Int, Int, Int) = (someNumbersInAnIntArray[0], someNumbersInAnIntArray[1], someNumbersInAnIntArray[2], someNumbersInAnIntArray[3])
+  
+  var valuesRect: CGRect = CGRectMake(CGFloat(someNumbersInAnIntArray[0]), CGFloat(someNumbersInAnIntArray[1]), CGFloat(someNumbersInAnIntArray[2]), CGFloat(someNumbersInAnIntArray[3]))
+  
+  var canvas: UIView = UIView(frame: valuesRect)
+  canvas.backgroundColor = UIColor.blueColor()
+  
+  //var label: UILabel = UILabel(frame: CGRectMake(50, 50, 100, 50))
+  var label: UILabel = UILabel()
+  label.text = "My Title"
+  label.textColor = UIColor.whiteColor()
+  label.textAlignment = NSTextAlignment.Center
+  label.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
+  
+  canvas.addSubview(label)
 }
 
-println(someNumbersInAnIntArray)
+func test02() {
+  var myString = "String"
+  var firstChar = myString.removeAtIndex(myString.startIndex)
+  myString
+}
 
-var someNumbersInAFourTuple: (Int, Int, Int, Int) = (someNumbersInAnIntArray[0], someNumbersInAnIntArray[1], someNumbersInAnIntArray[2], someNumbersInAnIntArray[3])
+func test03() {
+  var myString = "String"
+//  name.removeRange(name.startIndex..<advance(name.startIndex, 6))
+  myString.removeRange(myString.startIndex..<advance(myString.startIndex, 2))
+  myString
+}
 
-var valuesRect: CGRect = CGRectMake(CGFloat(someNumbersInAnIntArray[0]), CGFloat(someNumbersInAnIntArray[1]), CGFloat(someNumbersInAnIntArray[2]), CGFloat(someNumbersInAnIntArray[3]))
 
-var canvas: UIView = UIView(frame: valuesRect)
-canvas.backgroundColor = UIColor.blueColor()
-
-//var label: UILabel = UILabel(frame: CGRectMake(50, 50, 100, 50))
-var label: UILabel = UILabel()
-label.text = "My Title"
-label.textColor = UIColor.whiteColor()
-label.textAlignment = NSTextAlignment.Center
-label.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
-
-canvas.addSubview(label)
+//test01()
+//test02()
+test03()
