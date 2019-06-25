1d7aef1af37cfaf7f36844983470328e8be1b799
Tim Fuqua <timfuqua@gmail.com>
2019-06-24 21:12:48 -0700

    Adding stub files.

---
+++ DijkstrasAlgorithm/DijkstrasAlgorithm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DijkstrasAlgorithm
+//
+//  Created by Tim Fuqua on 6/24/19.
+//  Copyright © 2019 Norswedgian Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
